f694cefefb918507758a89a55c28045afcf7d9d8
Vietvp <vietvp892002@gmail.com>
2023-01-04 16:59:05 +0700

    fomat

--- local/th_dashboard/db/upgrade.php
+++ local/th_dashboard/db/upgrade.php
@@ -3,22 +3,22 @@
 defined('MOODLE_INTERNAL') || die;
 
 function xmldb_local_th_dashboard_upgrade($oldversion) {
-    global $CFG, $DB;
+	global $CFG, $DB;
 
-    $dbman = $DB->get_manager();
-    if (!$dbman->table_exists('local_th_dashboard')) {
+	$dbman = $DB->get_manager();
+	if (!$dbman->table_exists('local_th_dashboard')) {
 
-        $table = new xmldb_table('local_th_dashboard');
+		$table = new xmldb_table('local_th_dashboard');
 
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('content', XMLDB_TYPE_TEXT);
-        $table->add_field('userid', XMLDB_TYPE_INTEGER,  '10', null, null, null, 0);
-        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '15', null, null, null, 0);
+		$table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+		$table->add_field('content', XMLDB_TYPE_TEXT);
+		$table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, null, null, 0);
+		$table->add_field('timecreated', XMLDB_TYPE_INTEGER, '15', null, null, null, 0);
 
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+		$table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
 
-        $dbman->create_table($table);
-    }
+		$dbman->create_table($table);
+	}
 
-    return true;
+	return true;
 }
